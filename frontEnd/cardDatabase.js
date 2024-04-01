9c61d0557269f9ed07a8602077ec326a0c49eacf
clackclock <clackclick2020@gmail.com>
2024-04-01 18:19:29 -0400

    html work doing my best. making the java files work

---
+++ frontEnd/cardDatabase.js
@@ -0,0 +1,175 @@
+// Generated JS from Java: Game.cardDatabase -----
+function Game_cardDatabase() {
+   this.pack = null;
+   this.formPack = null;
+   jv_Object.call(this);
+   this._Game_cardDatabaseInit();
+}
+
+var Game_cardDatabase_c = sc_newClass("Game.cardDatabase", Game_cardDatabase, jv_Object, null);
+
+Game_cardDatabase_c.cardData = function ()  {
+   this.pack.add(new Card(-1, "Blank", sc_initArray(Number_c, 1, [ 0, 0 ]),
+              sc_initArray(Number_c, 1, [ 0, 0 ]), sc_initArray(Number_c, 1, [ 0, 0 ]),
+              sc_initArray(Number_c, 1, [ 0, 0 ]), sc_initArray(Number_c, 1, [ 0, 0 ]),
+              "NEUTRAL", "NEUTRAL"));
+   this.pack.add(new Card(0, "Basic_Seeds", sc_initArray(Number_c, 1, [ 4, 2 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 1, 4 ]),
+              "PLANT", "FARM"));
+   this.pack.add(new Card(1, "Simple_Axe", sc_initArray(Number_c, 1, [ 4, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 3, 5 ]), sc_initArray(Number_c, 1, [ 2, 4 ]),
+              "TOOL", "NEUTRAL"));
+   this.pack.add(new Card(2, "Feather", sc_initArray(Number_c, 1, [ 3, 1 ]),
+              sc_initArray(Number_c, 1, [ 3, 1 ]), sc_initArray(Number_c, 1, [ 4, 2 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 5, 3 ]),
+              "ANIMAL", "FARM"));
+   this.pack.add(new Card(3, "Flaccid_Flower", sc_initArray(Number_c, 1, [ 2, 1 ]),
+              sc_initArray(Number_c, 1, [ 1, 1 ]), sc_initArray(Number_c, 1, [ 1, 3 ]),
+              sc_initArray(Number_c, 1, [ 1, 3 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              "FLOWER", "PLANT"));
+   this.pack.add(new Card(4, "Gourd_Seeds", sc_initArray(Number_c, 1, [ 1, 4 ]),
+              sc_initArray(Number_c, 1, [ 1, 4 ]), sc_initArray(Number_c, 1, [ 2, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              "PLANT", "FARM"));
+   this.pack.add(new Card(5, "Pumpkin", sc_initArray(Number_c, 1, [ 2, 5 ]),
+              sc_initArray(Number_c, 1, [ 2, 5 ]), sc_initArray(Number_c, 1, [ 3, 5 ]),
+              sc_initArray(Number_c, 1, [ 3, 5 ]), sc_initArray(Number_c, 1, [ 3, 5 ]),
+              "PLANT", "FARM"));
+   this.pack.add(new Card(6, "Apple", sc_initArray(Number_c, 1, [ 3, 2 ]),
+              sc_initArray(Number_c, 1, [ 3, 2 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              "PLANT", "FARM"));
+   this.pack.add(new Card(7, "Mutton", sc_initArray(Number_c, 1, [ 4, 1 ]),
+              sc_initArray(Number_c, 1, [ 3, 2 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              "ANIMAL", "FARM"));
+   this.pack.add(new Card(8, "HayBale", sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 2, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 2, 5 ]),
+              "PLANT", "FARM"));
+   this.pack.add(new Card(9, "Shears", sc_initArray(Number_c, 1, [ 3, 1 ]),
+              sc_initArray(Number_c, 1, [ 2, 1 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              "ANIMAL", "FARM"));
+   this.pack.add(new Card(10, "Cornflower", sc_initArray(Number_c, 1, [ 2, 1 ]),
+              sc_initArray(Number_c, 1, [ 2, 1 ]), sc_initArray(Number_c, 1, [ 1, 1 ]),
+              sc_initArray(Number_c, 1, [ 1, 1 ]), sc_initArray(Number_c, 1, [ 1, 1 ]),
+              "FLOWER", "FARM"));
+   this.pack.add(new Card(11, "Sapling", sc_initArray(Number_c, 1, [ 3, 1 ]),
+              sc_initArray(Number_c, 1, [ 2, 1 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              "PLANT", "WOOD"));
+   this.pack.add(new Card(12, "Door", sc_initArray(Number_c, 1, [ 2, 1 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 3 ]), sc_initArray(Number_c, 1, [ 1, 3 ]),
+              "ITEM", "WOOD"));
+   this.pack.add(new Card(14, "Jeweled_Axe", sc_initArray(Number_c, 1, [ 5, 1 ]),
+              sc_initArray(Number_c, 1, [ 5, 1 ]), sc_initArray(Number_c, 1, [ 4, 2 ]),
+              sc_initArray(Number_c, 1, [ 4, 2 ]), sc_initArray(Number_c, 1, [ 4, 2 ]),
+              "TOOL", "LUCKY"));
+   this.pack.add(new Card(15, "Simple Spade", sc_initArray(Number_c, 1, [ 2, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 4 ]), sc_initArray(Number_c, 1, [ 3, 2 ]),
+              "TOOL", "NEUTRAL"));
+   this.pack.add(new Card(16, "Fishing_Rod", sc_initArray(Number_c, 1, [ 1, 1 ]),
+              sc_initArray(Number_c, 1, [ 1, 1 ]), sc_initArray(Number_c, 1, [ 1, 1 ]),
+              sc_initArray(Number_c, 1, [ 1, 1 ]), sc_initArray(Number_c, 1, [ 1, 1 ]),
+              "ITEM", "TOOL"));
+   this.pack.add(new Card(19, "Iron", sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "METAL", "NEUTRAL"));
+   this.pack.add(new Card(20, "Iron_Ore", sc_initArray(Number_c, 1, [ 1, 1 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 1, 1 ]),
+              "ORE", "METAL"));
+   this.pack.add(new Card(21, "Planks", sc_initArray(Number_c, 1, [ 3, 2 ]),
+              sc_initArray(Number_c, 1, [ 3, 2 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              "PLANT", "WOOD"));
+   this.pack.add(new Card(22, "Water_Bucket", sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "ITEM", "FARM"));
+   this.pack.add(new Card(23, "Gold", sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "METAL", "NEUTRAL"));
+   this.pack.add(new Card(24, "Anvil", sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 4, 3 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 3, 4 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "TOOL", "METAL"));
+   this.pack.add(new Card(25, "NameTag", sc_initArray(Number_c, 1, [ 1, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 1, 2 ]),
+              "ITEM", "NEUTRAL"));
+   this.pack.add(new Card(28, "Furnace", sc_initArray(Number_c, 1, [ 2, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 4, 4 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "TOOL", "NEUTRAL"));
+   this.pack.add(new Card(29, "Trident", sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "WATER", "TOOL"));
+   this.pack.add(new Card(30, "Healing_Potion", sc_initArray(Number_c, 1, [ 0, 1 ]),
+              sc_initArray(Number_c, 1, [ 0, 1 ]), sc_initArray(Number_c, 1, [ 1, 3 ]),
+              sc_initArray(Number_c, 1, [ 1, 3 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              "ITEM", "NEUTRAL"));
+   this.pack.add(new Card(31, "Wheat", sc_initArray(Number_c, 1, [ 2, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 2, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 2, 3 ]),
+              "PLANT", "FARM"));
+   this.pack.add(new Card(32, "Coral", sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 4, 4 ]), sc_initArray(Number_c, 1, [ 4, 5 ]),
+              "WATER", "PLANT"));
+   this.pack.add(new Card(33, "Kelp", sc_initArray(Number_c, 1, [ 2, 4 ]),
+              sc_initArray(Number_c, 1, [ 3, 4 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 4, 4 ]),
+              "WATER", "PLANT"));
+   this.pack.add(new Card(35, "Aquamarine", sc_initArray(Number_c, 1, [ 2, 3 ]),
+              sc_initArray(Number_c, 1, [ 3, 3 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 4, 4 ]), sc_initArray(Number_c, 1, [ 4, 5 ]),
+              "WATER", "METAL"));
+   this.pack.add(new Card(36, "Gold_Ore", sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 1 ]), sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 2, 2 ]),
+              "ORE", "NEUTRAL"));
+   this.pack.add(new Card(37, "Poppy", sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 2, 2 ]), sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 1 ]), sc_initArray(Number_c, 1, [ 2, 2 ]),
+              "FLOWER", "NEUTRAL"));
+   this.pack.add(new Card(38, "Dandelion", sc_initArray(Number_c, 1, [ 3, 2 ]),
+              sc_initArray(Number_c, 1, [ 3, 2 ]), sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 2, 1 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "FLOWER", "NEUTRAL"));
+   this.pack.add(new Card(39, "Raw_Steak", sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 3 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              "ANIMAL", "FARM"));
+   this.pack.add(new Card(40, "Wool", sc_initArray(Number_c, 1, [ 2, 2 ]),
+              sc_initArray(Number_c, 1, [ 1, 2 ]), sc_initArray(Number_c, 1, [ 3, 3 ]),
+              sc_initArray(Number_c, 1, [ 2, 4 ]), sc_initArray(Number_c, 1, [ 3, 4 ]),
+              "FARM", "ANIMAL"));
+   this.pack.add(new Card(41, "Newton's_Apple", sc_initArray(Number_c, 1, [ 4, 2 ]),
+              sc_initArray(Number_c, 1, [ 3, 1 ]), sc_initArray(Number_c, 1, [ 3, 1 ]),
+              sc_initArray(Number_c, 1, [ 4, 4 ]), sc_initArray(Number_c, 1, [ 1, 1 ]),
+              "PLANT", "LUCKY"));
+};
+Game_cardDatabase_c.formData = function ()  {
+   this.formPack.add(new ComboBuild(17, "Tree", sc_initArray(Number_c, 1, [ 2, 4 ]), "PLANT"));
+   this.formPack.add(new ComboBuild(18, "Flower_Bush", sc_initArray(Number_c, 1, [ 2, 2 ]), "FLOWER"));
+   this.formPack.add(new ComboBuild(26, "Item_Frame", sc_initArray(Number_c, 1, [ 2, 2 ]), "TOOL"));
+   this.formPack.add(new ComboBuild(27, "Stairs", sc_initArray(Number_c, 1, [ 1, 1 ]), "TOOL"));
+   this.formPack.add(new ComboBuild(34, "Ocean_Monument", sc_initArray(Number_c, 1, [ 3, 3 ]), "WATER"));
+   this.formPack.add(new ComboBuild(42, "Apple_Pie", sc_initArray(Number_c, 1, [ 5, 5 ]), "NEUTRAL"));
+};
+
+Game_cardDatabase_c._Game_cardDatabaseInit = function() {
+   this.pack = new jv_ArrayList();
+      ;
+   this.formPack = new jv_ArrayList();
+      ;
+};
