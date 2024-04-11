eb4dbab52225f81f19cdf83786f87b53b4441601
clackclock <clackclick2020@gmail.com>
2024-04-11 17:14:56 -0400

    attempting merge of posmap stuff and js board edits// might use jsp later

--- src/Game/Slots/PosMap.java
+++ src/Game/Slots/PosMap.java
@@ -5,10 +5,7 @@ import java.util.HashMap;
 import static Game.Slots.Board.Board_Positions.*;
 
 public class PosMap {
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
     private final HashMap<Board.Board_Positions, int[]> positionMap = new HashMap<>();
 
     public PosMap(int[] uAtk, int[] atk, int[] cDef, int[] corn, int[] def) {
@@ -22,8 +19,6 @@ public class PosMap {
     public int[] row(Board.Board_Positions pos) {
         return positionMap.get(pos);
     }
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> origin/master
+
