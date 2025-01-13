6984d95469bd652d17ea67f1fcacd16ddc37beb0
clackclock <clackclick2020@gmail.com>
2025-01-13 14:52:38 -0500

    /hello is broken want to cry

--- demo/src/main/java/chesticuffsJavaEdition/demo/ChestCardGameApplication.java
+++ demo/src/main/java/chesticuffsJavaEdition/demo/ChestCardGameApplication.java
@@ -2,6 +2,8 @@ package chesticuffsJavaEdition.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,5 +14,10 @@ public class ChestCardGameApplication {
 		SpringApplication.run(ChestCardGameApplication.class, args);
 	}
 	//http://localhost:8080/hello
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 
 }
