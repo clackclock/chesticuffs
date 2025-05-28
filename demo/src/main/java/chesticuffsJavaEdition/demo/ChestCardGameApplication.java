555039d76d389f576679e1f2408b7ce22bfedca4
clackclock <clackclick2020@gmail.com>
2025-05-28 15:06:26 -0400

    springboot adjustment

--- demo/src/main/java/chesticuffsJavaEdition/demo/ChestCardGameApplication.java
+++ demo/src/main/java/chesticuffsJavaEdition/demo/ChestCardGameApplication.java
@@ -2,6 +2,7 @@ package chesticuffsJavaEdition.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,6 @@ public class ChestCardGameApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
 
 }
